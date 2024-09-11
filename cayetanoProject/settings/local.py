0f857ef93f92c8c47da5fc18d506076fc67f4ec5
Joaquín Lopez <joaquinacho@live.com>
2024-09-11 18:15:18 -0300

    Base de datos en la nube, conexión con PostgreSQL

--- cayetanoProject/settings/local.py
+++ cayetanoProject/settings/local.py
@@ -11,7 +11,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'PORT': '5432'
     }
 }
 
